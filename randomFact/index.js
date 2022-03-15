1bf17a24828785b80edfb8aa54c7d1a378776ba5
jackengtwistio <jtwist108@gmail.com>
2022-03-15 11:19:11 -0400

    deleted redundant file

--- randomFact/index.js
+++
@@ -1,9 +0,0 @@
-import { facts } from "./data.js";
-
-let text = document.querySelector("p");
-let button = document.querySelector("button");
-
-function showAFact() {
-  text.innerHTML = facts[Math.floor(Math.random() * facts.length)];
-}
-button.onclick = showAFact;
