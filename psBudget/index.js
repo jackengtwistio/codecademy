8f0de669905ef5ec0a8606d2b663c5dafdb7eda3
jackengtwistio <jtwist108@gmail.com>
2022-03-17 23:45:16 -0400

    post envelop id value

--- psBudget/index.js
+++ psBudget/index.js
@@ -6,12 +6,22 @@ app.get("/", (req, res) => {
   res.send(["Hello World!", envelopes]);
 });
 app.post("/", (req, res) => {
-  const newEnv = req.query.envelope;
-  envelopes.push(newEnv);
-  res.send(newEnv);
+  const { envelope, id, value } = req.query;
+  // console.log(envelopes.every((e) => e.id !== id));
+  if (!id || !envelopes.every((e) => e.id !== id)) {
+    return res.status(400).send("bad request");
+  }
+  envelopes.push({ envelope, id, value });
+  res.send(envelopes);
   console.log(envelopes);
 });
-
+app.param("id", (req, res, next, id) => {
+  req.id = id;
+  next();
+});
+app.get("/:id", (req, res) => {
+  return res.send(envelopes[req.id]);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
