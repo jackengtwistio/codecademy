bf34843848af8cef01c1cfb206bcbc3befef108e
jackengtwistio <jtwist108@gmail.com>
2022-03-17 23:16:41 -0400

    post envelop, get envelops

--- psBudget/index.js
+++ psBudget/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const envelopes = [];
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(["Hello World!", envelopes]);
+});
+app.post("/", (req, res) => {
+  const newEnv = req.query.envelope;
+  envelopes.push(newEnv);
+  res.send(newEnv);
+  console.log(envelopes);
 });
 
 app.listen(port, () => {
