763c5409d5ba5b7f6d9b16cdc68aca3ab3ab13c4
jackengtwistio <jtwist108@gmail.com>
2022-03-18 21:58:35 -0400

    put delete by id, transfer value amount between two

--- psBudget/index.js
+++ psBudget/index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const envelopes = [];
-app.get("/", (req, res) => {
-  res.send(["Hello World!", envelopes]);
-});
-app.post("/", (req, res) => {
-  const { envelope, id, value } = req.query;
-  // console.log(envelopes.every((e) => e.id !== id));
-  if (!id || !envelopes.every((e) => e.id !== id)) {
-    return res.status(400).send("bad request");
+const envelopes = {};
+
+app.post("/transfer/:from/:to/:amount", function (req, res) {
+  let { from, to, amount } = req.params;
+  amount = Number(amount);
+  if (envelopes[from] && envelopes[to]) {
+    envelopes[from].value = Number(envelopes[from].value) - amount;
+    envelopes[to].value = Number(envelopes[to].value) + amount;
+    return res.send(envelopes);
   }
-  envelopes.push({ envelope, id, value });
-  res.send(envelopes);
-  console.log(envelopes);
+  res
+    .status(404)
+    .send([
+      !envelopes[from] && `can not find${from}`,
+      !envelopes[to] && `can not find${to}`,
+    ]);
 });
+
 app.param("id", (req, res, next, id) => {
   req.id = id;
   next();
@@ -22,6 +26,33 @@ app.param("id", (req, res, next, id) => {
 app.get("/:id", (req, res) => {
   return res.send(envelopes[req.id]);
 });
+app.put("/:id", (req, res) => {
+  const { id, value, name } = req.query;
+  envelopes[id] = { value, name };
+  res.send(`envelop ${id} updated to ${{ ...envelopes[id] }}`);
+});
+app.delete("/:id", (req, res) => {
+  if (envelopes[req.id]) {
+    delete envelopes[req.id];
+    res.send(envelopes);
+  } else {
+    res.status(404).send(`can not find ${req.id}`);
+  }
+});
+app.get("/", (req, res) => {
+  res.send(["Hello World!", envelopes]);
+});
+app.post("/", (req, res) => {
+  const { name, id, value } = req.query;
+  // console.log(envelopes.every((e) => e.id !== id));
+  if (!id || Object.keys(envelopes).includes(id)) {
+    return res.status(400).send("bad request");
+  }
+  envelopes[id] = { name, value };
+  res.send(envelopes);
+  console.log(envelopes);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
